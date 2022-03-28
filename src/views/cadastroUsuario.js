15e10f2fbd8f8999ef33e50fc6529d84cf338e12
Jose Milton Junior <jmjuniormonteiro@gmail.com>
2022-03-28 11:10:36 -0300

    Criando o component Cadastro de Usuario

---
+++ src/views/cadastroUsuario.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Card from '../components/card';
+
+class CadastroUsuario extends React.Component{
+
+    render(){
+        return (
+            <Card title='Cadastro de Usuário'>
+                Olá
+            </Card>
+        )
+    }
+}
+
+export default CadastroUsuario;
