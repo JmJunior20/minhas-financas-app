2ccf60a1df58bc1108c49897d68b96a398ae6363
Jose Milton Junior <jmjuniormonteiro@gmail.com>
2022-03-29 18:46:07 -0300

    Importando o FormGroup

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 import Card from '../components/card';
+import FormGroup from '../components/form-group';
 
 class CadastroUsuario extends React.Component{
 
     render(){
         return (
-            <Card title='Cadastro de Usuário'>
-                Olá
-            </Card>
+            <div className="container">
+                <Card title='Cadastro de Usuário'>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
         )
     }
 }
